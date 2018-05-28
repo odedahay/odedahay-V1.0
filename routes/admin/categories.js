ccc814a8ae0507304892089b1ff84122701c3ee7
Rodelio <odedahay@gmail.com>
2018-05-28 13:01:36 +0800

    update category selection

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Category = require('../../models/Category');
+const Post = require('../../models/Post');
 
 // To ensure layout of Admin
 router.all('/*', (req, res, next) => {
@@ -12,23 +13,71 @@ router.all('/*', (req, res, next) => {
 
 router.get('/', (req, res) => {
     Category.find({})
-    .then( categories =>{
-        res.render('admin/categories/index', {
-            categories: categories
-        });
-    })
-    
+        .then(categories => {
+            res.render('admin/categories/index', {
+                categories: categories
+            });
+        })
+
 });
 
-router.post('/create', (req, res)=>{
+router.post('/create', (req, res) => {
     const newCategory = new Category({
         name: req.body.name
     });
 
-    newCategory.save( saveCategory =>{
+    newCategory.save(saveCategory => {
         res.redirect('/admin/categories');
     });
 
+});
+
+// Edit Category
+router.get('/edit/:id', (req, res) => {
+    Category.findOne({ _id: req.params.id })
+        .then(category => {
+            res.render('admin/categories/edit', {
+                category: category
+            });
+        })
+});
+
+// Update Category
+router.put('/edit/:id', (req, res) => {
+    Category.findOne({ _id: req.params.id })
+        .then(category => {
+            category.name = req.body.name;
+            category.save()
+                .then(savedCategory => {
+                    req.flash('success_message', 'Category was successfully updated');
+                    res.redirect('/admin/categories')
+                })
+        })
+});
+
+// Delete Category
+router.delete('/:id', (req, res) => {
+    Category.remove({ _id: req.params.id })
+        .then(result => {
+            req.flash('success_message', 'Categogry was successfully deleted');
+            res.redirect('/admin/categories');
+        })
+});
+
+// Get all Categories 
+router.get('/:catId', (req, res) => {
+    Post.find({ category: req.params.catId })
+        .populate('category')
+        .then(posts => {
+            Category.findOne({ id: req.params.id })
+                .then(category => {
+                    res.render('admin/posts/categories', {
+                        posts: posts,
+                        category: category
+                    })
+                })
+        })
+        .catch(err => console.log(err));
 });
 
 
--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
+const Category = require('../../models/Category');
 const { isEmpty, uploadDir } = require('../../helpers/upload-helper');
 const fs = require('fs');
 
@@ -12,6 +13,7 @@ router.all('/*', (req, res, next) => {
 router.get('/', (req, res) => {
 
     Post.find({})
+        .populate('category')
         .then((posts) => {
             res.render('admin/posts/posts', {
                 posts: posts
@@ -20,7 +22,15 @@ router.get('/', (req, res) => {
 });
 
 router.get('/create', (req, res) => {
-    res.render('admin/posts/create');
+    Category.find({})
+        .then(categories => {
+            res.render('admin/posts/create',
+                {
+                    categories: categories
+                }
+            );
+        });
+
 });
 
 router.post('/create', (req, res) => {
@@ -70,13 +80,13 @@ router.post('/create', (req, res) => {
             status: req.body.status,
             allowComments: allowComments,
             body: req.body.body,
-
+            category: req.body.category,
             file: filename
 
         });
 
         newPost.save()
-            .then(() =>{
+            .then(() => {
                 req.flash('success_message', 'Post was successfully created');
                 res.redirect('/admin/posts')
             })
@@ -89,10 +99,15 @@ router.post('/create', (req, res) => {
 router.get('/edit/:id', (req, res) => {
     Post.findOne({ _id: req.params.id })
         .then((post) => {
-            res.render('admin/posts/edit', {
-                post: post
-            })
-        })
+
+            Category.find({})
+                .then(categories => {
+                    res.render('admin/posts/edit', {
+                        post: post,
+                        categories: categories
+                    })
+                });
+        });
 });
 
 // Update the Post
@@ -111,6 +126,7 @@ router.put('/edit/:id', (req, res) => {
             post.title = req.body.title;
             post.status = req.body.status;
             post.body = req.body.body;
+            post.category = req.body.category;
 
             if (!isEmpty(req.files)) {
 
@@ -137,7 +153,7 @@ router.put('/edit/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     Post.findOne({ _id: req.params.id })
-        .then( post => {
+        .then(post => {
 
             fs.unlink(uploadDir + post.file, (err) => {
 
